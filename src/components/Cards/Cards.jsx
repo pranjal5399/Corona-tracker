4ea0755930c3a333ddb1f80c25949cb7899e096a
pranjal5399 <pranjalchourasia2915@gmail.com>
2020-08-07 19:44:46 +0530

    spinner added

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import { Card, CardContent, Typography, Grid, CircularProgress } from '@material-ui/core';
 import CountUp from 'react-countup';
 import cx from 'classnames';
 import styles from './Cards.module.css'
 
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
     if (!confirmed) {
-        return "Loading...";
+        return (<div className={styles.circularProgress}>
+            <CircularProgress />
+        </div>);
     }
     return (
         <div className={styles.container}>
@@ -16,7 +18,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant="h5"><CountUp start={0} end={confirmed.value} duration={2.5} separator="," /></Typography>
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-                        <Typography variant="body2">Total Number of active cases of COVID-19:</Typography>
+                        <Typography variant="body2">Total Number of active cases of COVID-19.</Typography>
                     </CardContent>
                 </Grid>
 
@@ -25,7 +27,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                         <Typography variant="h5"><CountUp start={0} end={recovered.value} duration={2.5} separator="," /></Typography>
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-                        <Typography variant="body2">Total Number of recoveries from COVID-19:</Typography>
+                        <Typography variant="body2">Total Number of recoveries from COVID-19.</Typography>
                     </CardContent>
                 </Grid>
 
@@ -34,7 +36,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                         <Typography variant="h5"><CountUp start={0} end={deaths.value} duration={2.5} separator="," /></Typography>
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-                        <Typography variant="body2">Total Number of deaths caused by COVID-19:</Typography>
+                        <Typography variant="body2">Total Number of deaths caused by COVID-19.</Typography>
                     </CardContent>
                 </Grid>
             </Grid>
